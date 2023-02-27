08c458c65b8f8c9465e537535ca99a273acfcf96
SheiKhaAhm laptop <itzshakeey@gmail.com>
2023-02-27 16:07:57 +0000

    asyn function for error handling

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const fruitForm = document.querySelector("#inputSection form");
 const fruitList = document.querySelector("#fruitSection ul");
 const fruitNutrition = document.querySelector("#nutritionSection p")
 
+const fruitNutritionImages = document.querySelector("#nutritionSection img")
+const APIkey = '33986170-cb342e36e814b5e7e54b0ada5'
+
+
+
+
 fruitForm.addEventListener("submit", extractFruit);
 
 // fruitList.addEventListener("click", (e) => console.log(e.target))
@@ -40,29 +46,44 @@ function addFruit(fruit) {
     fruitList.appendChild(li);
 
     fruitNutrition.textContent = `Calorie count: ${cal += fruit.nutritions.calories}`
-
-
 };
+
 
 function removeFruit(e) {
     e.target.remove();
 }
 
 
-function fetchFruitData(fruit) {
-    fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
-        .then(resp => processResponse(resp))
-        .then(data => addFruit(data))
-        .catch((e) => console.log(e))
+async function fetchFruitData(fruit) {
+    try {
+        const resp = await fetch(`https://fruity-api.onrender.com/fruits/${fruit}`);
+        if (resp.ok) {
+            const data = await resp.json()
+            addFruit(data)
+        }
+        else {
+            throw `Error; http status code = ${resp.status}`
+        }
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 
-function processResponse(resp) {
-    if (resp.ok) {
-        return resp.json()
-    }
-    else {
-        throw `Error; http status code = ${resp.status}`
-    }
-}
+// function fetchFruitData(fruit) {
+//     fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
+//         .then(resp => processResponse(resp))
+//         .then(data => addFruit(data))
+//         .catch((e) => console.log(e))
+// }
+
+
+// function processResponse(resp) {
+//     if (resp.ok) {
+//         return resp.json()
+//     }
+//     else {
+//         throw `Error; http status code = ${resp.status}`
+//     }
+// }
 
