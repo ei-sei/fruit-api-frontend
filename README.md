8251cd4ed5b46060ed0bb29cd3ac9c93acb344e0
SheiKhaAhm laptop <itzshakeey@gmail.com>
2023-02-28 21:10:23 +0000

    implementing new form with post route

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 npm i
 npm run dev
 -To start the bundle.js
+
+
+Image API pulled from https://pixabay.com/api/docs/#api_key
--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// require('dotenv').config();
 const fruitForm = document.querySelector("#inputSection form");
 const fruitList = document.querySelector("#fruitSection ul");
 const fruitNutrition = document.querySelector("#nutritionSection p");
 
+
+
 const APIkey = '33986170-cb342e36e814b5e7e54b0ada5'
+// const APIkey = process.env.api;
 
 fruitForm.addEventListener("submit", extractFruit);
 let cal = 0;
@@ -24,12 +28,12 @@ function addFruit(fruit, fruitImage) {
     img.alt = fruit.name;
     img.src = fruitImage.hits[0].previewURL;
 
-    img.addEventListener("click", removeFruit, {once: true});
+    img.addEventListener("click", removeFruit, { once: true });
     fruitList.appendChild(img);
 
     fruitCal[fruit.name] = fruit.nutritions.calories;
 
-    
+
     cal += fruit.nutritions.calories;
     fruitNutrition.textContent = `Calorie count: ${cal}`;
 };
@@ -47,7 +51,8 @@ function removeFruit(e) {
 
 async function fetchFruitData(fruit) {
     try {
-        const respData = await fetch(`https://fruity-api.onrender.com/fruits/${fruit}`);
+        // const respData = await fetch(`https://fruity-api.onrender.com/fruits/${fruit}`);
+        const respData = await fetch(`https://fruit-api-6m8m.onrender.com/fruits/${fruit}`);
         const respImage = await fetch(`https://pixabay.com/api/?key=${APIkey}&q=${fruit}&image_type=photo&pretty=true`);
         if (respData.ok && respImage.ok) {
             const data = await respData.json();
@@ -61,3 +66,28 @@ async function fetchFruitData(fruit) {
         console.log(e);
     }
 }
+
+const createNewFruit = async (e) => {
+
+    e.preventDefault();
+
+    const data = {name: e.target.newFruitInput.value }
+
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify(data)
+    }
+
+
+    const response = await fetch("https://fruit-api-6m8m.onrender.com/fruits", options);
+
+    if (response.status === 201) {
+        e.target.newFruitInput.value = ''
+    }
+}
+
+const newFruitForm = document.querySelector("#create-form");
+newFruitForm.addEventListener('submit', createNewFruit);    
